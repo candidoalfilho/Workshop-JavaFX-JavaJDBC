817520666528440857afb8d71822906e66c861aa
Candido Alfredo <candidoalfilho@hotmail.com>
2021-01-28 14:32:51 -0300

    Adding database acess

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,11 +5,12 @@ import java.io.Serializable;
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-	
 	private Integer id;
 	private String name;
 	
-	public Department() {}
+	public Department() {
+		
+	}
 
 	public Department(Integer id, String name) {
 		this.id = id;
@@ -33,16 +34,10 @@ public class Department implements Serializable {
 	}
 
 	@Override
-	public String toString() {
-		return "Department [id=" + id + ", name=" + name + "]";
-	}
-
-	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
 
@@ -60,16 +55,12 @@ public class Department implements Serializable {
 				return false;
 		} else if (!id.equals(other.id))
 			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
-			return false;
 		return true;
 	}
-	
-	
-	
-	
 
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
+	}
+	
 }
